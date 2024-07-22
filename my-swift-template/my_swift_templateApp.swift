7045b60b1f478f092c1dc9b2aa4f3db606a9317f
GrandNewStart <hbjw123@naver.com>
2024-07-22 10:54:11 +0900

    Initial Commit

---
+++ my-swift-template/my_swift_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_swift_templateApp.swift
+//  my-swift-template
+//
+//  Created by Jinwoo Hwangbo on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct my_swift_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
